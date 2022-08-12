b1a898a83f15983e2bc4942c8c2412930cc0c3e1
skdat6 <dobretheo@gmail.com>
2022-08-12 16:28:38 +0300

    files added, desc

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rentals-finder-sheet
  
+Scraping a local website to find desired apartments to rent based on filters
+
+Needs to put scrapped data into a sheet through a form
---
+++ main.py
@@ -0,0 +1,86 @@
+import time
+
+import selenium.common.exceptions
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+
+options = Options()
+options.add_argument("start-maximized")
+options.add_experimental_option("detach", True)
+#driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+#driver.get("https://www.imobiliare.ro/")
+
+
+class Apartment:
+    def __init__(self):
+        self.driver_ap = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        self.driver_ap.get('https://www.imobiliare.ro/')
+        self.ap_link = ''
+        self.ap_pret = 0
+
+    def get_link(self):
+        pass
+
+    def get_price(self):
+        pass
+
+    def set_filters(self, pret_max, nr_camere): #0 for oricate
+        self.driver_ap.find_element(By.CSS_SELECTOR, '#btn_vezi_filtrele > div.button.btn_collapse').click()
+        try:
+            pret_drop_down = self.driver_ap.find_element(By.XPATH, '//*[@id="form_filtre"]/div[1]/div[1]/div[1]/div/div/div/div[3]/div/button')
+            pret_drop_down.click()
+        except selenium.common.exceptions.ElementNotInteractableException:
+            time.sleep(3)
+            self.driver_ap.find_element(By.CSS_SELECTOR, '#btn_vezi_filtrele > div.button.btn_collapse').click()
+            pret_drop_down = self.driver_ap.find_element(By.XPATH,
+                                                         '//*[@id="form_filtre"]/div[1]/div[1]/div[1]/div/div/div/div[3]/div/button')
+            pret_drop_down.click()
+
+        correct_price = int(pret_max / 50)
+        price_selection = self.driver_ap.find_element(By.XPATH, f'//*[@id="bs-select-2-{correct_price-1}"]')
+        price_selection.click()
+
+        camere = int(nr_camere + 1)
+        camere_selection = self.driver_ap.find_element(By.XPATH, f'// *[ @ id = "form_filtre"] / div[1] / div[1] / div[3] / div / div / label[{camere}]')
+        camere_selection.click()
+
+        self.driver_ap.find_element(By.XPATH, '//*[@id="form_filtre"]/div[2]/div[2]/div/a').click()
+
+
+ap = Apartment()
+time.sleep(3)
+
+accept_cookies = ap.driver_ap.find_element(By.XPATH, '//*[@id="modCookies"]/div/div/div/div[2]/div[2]/div[3]/a')
+accept_cookies.click()
+
+locatie = ap.driver_ap.find_element(By.XPATH, '//*[@id="b_cautator_locatie_val"]')
+locatie.clear()
+locatie.send_keys("Bucuresti")
+time.sleep(2)
+
+type_select = ap.driver_ap.find_element(By.XPATH, '//*[@id="b_cautator_form"]/div/div[2]/div[1]/div[2]/button')
+type_select.click()
+type_select = ap.driver_ap.find_element(By.XPATH, '//*[@id="b_cautator_form"]/div/div[2]/div[1]/div[2]/ul/li[1]/ul/li[1]/a')
+type_select.click()
+
+in_chirie = ap.driver_ap.find_element(By.XPATH, '//*[@id="b_cautator_form"]/div/div[2]/div[2]/div/button')
+in_chirie.click()
+in_chirie = ap.driver_ap.find_element(By.XPATH, '//*[@id="b_cautator_form"]/div/div[2]/div[2]/div/div/ul/li[2]/a')
+in_chirie.click()
+
+ap.driver_ap.find_element(By.CSS_SELECTOR, '#b_cautator_form > div > div.box-buton-cauta > input').click()
+
+ap.set_filters(300, 3)
+link = ap.driver_ap.find_element(By.XPATH, '//*[contains(@id,"anunt")]/div').text
+print(link)
+ap_list_names = ap.driver_ap.find_elements(By.XPATH, '//*[contains(@id,"anunt")]/div/div[1]/div[2]/div[1]/div[1]/div[2]/h2/span')
+try:
+    for aps in ap_list_names:
+        print(aps.text)
+except selenium.common.exceptions.StaleElementReferenceException:
+    ap.driver_ap.find_element(By.XPATH, '//*[@id="buton-exit-modal-imoagent"]').click()
+    ap.driver_ap.find_element(By.XPATH, '// *[ @ id = "auth-go-back"]').click()
